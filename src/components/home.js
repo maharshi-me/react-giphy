fb501ee8e56aee24f4eaff38d9afdd808904cfe9
Maharshi <maharshi3781@gmail.com>
2021-07-10 11:01:44 +0530

    added Text input post

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
-import { Navbar, Row, Col, Card, CardTitle, Icon, Button} from 'react-materialize';
-
+import { Navbar, Row, Col, Card, CardTitle, Icon, Button, Textarea} from 'react-materialize';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import './home.css';
 
 const myStyle = {
     mainCard:{
@@ -18,6 +23,11 @@ const myStyle = {
         backgroundColor:"#F0F2F5", 
         borderRadius:"50px", 
         cursor:"pointer"
+    },
+    submitButton:{
+        width:"100%", 
+        borderRadius:"8px", 
+        backgroundColor:"#1771E6"
     }
     
 }
@@ -26,14 +36,23 @@ class Home extends Component {
     constructor(props){
         super(props)
         this.state = {
+            open:false,
+            currentText:"",
             posts:[
                 {
-                    "text":"Default Post"
+                    "text":"This is default post"
                 },
             ]
         }
     }
+    handleClose = () => {
+        this.setState({open:false})
+    }
+    addPost = () => {
+        this.setState({posts:[{text:this.state.currentText}, ...this.state.posts], currentText:"", open:false})
+    }
     render() {
+        console.log(this.state)
         return (
             <>
                 <Navbar style={{backgroundColor:"white"}} />
@@ -42,12 +61,36 @@ class Home extends Component {
                         <Card style={myStyle.mainCard}>
                             <Row style={myStyle.noMargin}>
                                 <Col s={12} style={myStyle.noPadding}>
-                                    <div style={myStyle.formBox}>
+                                    <div style={myStyle.formBox} onClick={() => this.setState({open:true})}>
                                         What's on your mind?
                                     </div>
                                 </Col>
                             </Row>
                         </Card>
+                        <Dialog
+                            open={this.state.open}
+                            onClose={this.handleClose}
+                        >
+                            <div style={{textAlign:"center"}}>
+                                <h5>
+                                    Create Post
+                                </h5>
+                            </div>
+                            
+                            <DialogContent style={{width:"500px"}}>
+                                <Textarea
+                                    id="post-input"
+                                    placeholder="What's on your mind?"
+                                    value={this.state.currentText}
+                                    onChange={(e) => {this.setState({currentText:e.target.value})}}
+                                />
+                            </DialogContent>
+                            <DialogActions>
+                                <Button disabled={(this.state.currentText == "")?true:false} style={myStyle.submitButton} onClick={this.addPost}>
+                                    Post
+                                </Button>
+                            </DialogActions>
+                        </Dialog>
                         {this.state.posts.map((post, index) => {
                             return(
                                 <Card key={index} style={myStyle.mainCard}>
