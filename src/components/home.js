728b16b6f70439c58f4e12032b775e7f0d408b2d
Maharshi <maharshi3781@gmail.com>
2021-07-10 23:20:44 +0530

    added search query

--- src/components/home.js
+++ src/components/home.js
@@ -1,22 +1,17 @@
-import React, { Component } from 'react';
-import { Navbar, Row, Col, Card, CardTitle, Icon, Button, Textarea} from 'react-materialize';
+import React, { Component, useState } from 'react';
+import { Navbar, Row, Col, Card, Button, Textarea, Preloader} from 'react-materialize';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import GifIcon from '@material-ui/icons/Gif';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Chip from '@material-ui/core/Chip';
-import FaceIcon from '@material-ui/icons/Face';
-import DoneIcon from '@material-ui/icons/Done';
-import AddIcon from '@material-ui/icons/Add';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import SearchIcon from '@material-ui/icons/Search';
 import InputBase from '@material-ui/core/InputBase';
-
 import './home.css';
+
+const GiphyApiKey = "EQh0DeRaL6DoipBy8vrLyDzp1hF7f2ej"
 
 const myStyle = {
     mainCard:{
@@ -34,9 +29,6 @@ const myStyle = {
         textAlign:"center",
         padding:"10px 0px 0px 0px",
         cursor:"pointer",
-        "hover":{
-            backgroundColor:"rgba(200,200,200,0.2)"
-        }
     },
     formBox:{
         padding:"10px 15px",
@@ -53,9 +45,6 @@ const myStyle = {
         position: 'relative',
         borderRadius: "50px",
         backgroundColor: "rgba(200,200,200, 0.15)",
-        '&:hover': {
-          backgroundColor: "rgba(200,200,200, 0.25)",
-        },
         margin: "15px 24px 15px 24px",
         padding:"5px",        
     },
@@ -66,13 +55,24 @@ const myStyle = {
     },
     iconButton:{
         backgroundColor:"rgba(200,200,200, 0.15)", 
-        '&:hover': {
-            backgroundColor: "rgba(200,200,200, 0.5)",
-        },
         marginLeft:"24px", 
         marginRight:"24px", 
         marginTop:"15px",
-        padding:"5px"
+        padding:"5px",
+    },
+    iconButtonHover:{
+        backgroundColor: "rgba(200,200,200, 1)",
+        marginLeft:"24px", 
+        marginRight:"24px", 
+        marginTop:"15px",
+        padding:"5px",
+    },
+    removeGifButton:{
+        backgroundColor:"rgba(255,255,255, 0.95)", 
+        marginLeft:"24px",
+        marginRight:"24px", 
+        marginTop:"15px",
+        padding:"5px",
     },
     normalChip:{
 
@@ -86,6 +86,20 @@ const myStyle = {
     
 }
 
+const SmallIconButton = (props) => {
+    const [Hovered, setHovered] = useState(false);
+    return(
+        <IconButton 
+            style={Hovered?myStyle.iconButtonHover:myStyle.iconButton} 
+            onClick={props.onClick}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+        >
+            {props.icon}
+        </IconButton>
+    )
+}
+
 class Home extends Component {
     constructor(props){
         super(props)
@@ -94,6 +108,8 @@ class Home extends Component {
             isGifPage:false,
             currentText:"",
             currentGif:"",
+            gifQuery:"",
+            gifsLoading:false,
             gifs:[],
             posts:[
                 {
@@ -102,18 +118,54 @@ class Home extends Component {
                 },
             ]
         }
+        this.controller = null
     }
     componentDidMount(){
-        fetch('https://api.giphy.com/v1/gifs/trending?api_key=EQh0DeRaL6DoipBy8vrLyDzp1hF7f2ej&limit=12&rating=g')
+        this.fetchTrendingGifs();
+    }
+    fetchTrendingGifs(){
+        const trendingUrl = `https://api.giphy.com/v1/gifs/trending?api_key=${GiphyApiKey}&limit=12&rating=g`
+        this.setState({gifsLoading:true})
+        fetch(trendingUrl)
         .then(response => response.json())
         .then(data => 
             {
                 let gifs = []
                 data.data.forEach(gif => {
-                    gifs = [...gifs, gif["images"]["original"]["url"]]
+                    gifs = [...gifs, {small:gif["images"]["downsized_medium"]["url"],original:gif["images"]["downsized_medium"]["url"]}]
                 });
-                this.setState({gifs:gifs})
-            });
+                this.setState({gifs:gifs, gifsLoading:false})
+            })
+        .catch(function(err) {
+            console.error(` Err: ${err}`);
+        });
+    }
+    handleGifQueryChange = (e) => {
+        this.setState({gifQuery:e.target.value, gifsLoading:true})
+        // const controller = new AbortController()
+        // const signal = controller.signal
+        if (this.controller){
+            this.controller.abort()
+        }
+        if (e.target.value === ""){
+            this.fetchTrendingGifs();
+            return
+        }
+        this.controller = new AbortController()
+        const searchUrl = `https://api.giphy.com/v1/gifs/search?api_key=${GiphyApiKey}&limit=12&rating=g&q=${e.target.value}`
+        fetch(searchUrl,{method:"get", signal:this.controller.signal})
+        .then(response => response.json())
+        .then(data => 
+            {
+                let gifs = []
+                data.data.forEach(gif => {
+                    gifs = [...gifs, {small:gif["images"]["downsized_medium"]["url"],original:gif["images"]["downsized_medium"]["url"]}]
+                });
+                this.setState({gifs:gifs, gifsLoading:false})
+            })
+        .catch(function(err) {
+            console.error(` Err: ${err}`);
+        });
     }
     handleClose = () => {
         this.setState({open:false})
@@ -123,6 +175,30 @@ class Home extends Component {
     }
     render() {
         console.log(this.state)
+        let gifsList = null
+        if(this.state.gifsLoading){
+            gifsList = 
+            <Col s={12}>
+                <Preloader
+                active
+                color="blue"
+                flashing={false}
+                size="small"
+                />
+            </Col>
+            
+        }
+        else{
+            gifsList = this.state.gifs.map((gif, index) => {
+                return(
+                    <Col key={index} s={12}>
+                        <img style={{cursor:"pointer"}} width="100%" src={gif.small} alt={"Gif"} onClick={() => this.setState({currentGif:gif.original, isGifPage:false})}></img>
+                    </Col>
+                )
+            })
+        }
+        
+        
         return (
             <>
                 <Navbar style={{backgroundColor:"#1771E6"}} />
@@ -146,21 +222,19 @@ class Home extends Component {
                             ?
                             <>
                                 <div style={{position:"absolute", left:0, top:0}}>
-                                    <IconButton style={myStyle.iconButton} onClick={() => {this.setState({isGifPage:false})}}>
-                                        <ArrowBackIcon />
-                                    </IconButton>
+                                    <SmallIconButton key="gif-page" onClick={() => this.setState({isGifPage:false})} icon={<ArrowBackIcon/>} />
                                 </div>
                                 <div style={{textAlign:"center"}}>
                                     <h5>
                                         Choose a GIF
                                     </h5>
                                     <div style={myStyle.search}>
-                                    
                                         <InputBase
+                                            value={this.state.gifQuery}
+                                            onChange={this.handleGifQueryChange}
                                             autoFocus
                                             placeholder="Search…"
                                             style={myStyle.inputInput}
-                                            inputProps={{ 'aria-label': 'search' }}
                                         />
                                     </div>
                                 </div>
@@ -168,23 +242,14 @@ class Home extends Component {
                                 <DialogContent>
                                 
                                     <Row>
-                                        {this.state.gifs.map((gif, index) => {
-                                            return(
-                                                <Col key={index} s={12}>
-                                                    <img style={{cursor:"pointer"}} width="100%" src={gif} onClick={() => this.setState({currentGif:gif, isGifPage:false})}></img>
-                                                </Col>
-                                            )
-                                        })}
-                                        
+                                        {gifsList}
                                     </Row>
                                 </DialogContent>
                             </>
                             :
                             <>
                                 <div style={{position:"absolute",right:0, top:0}}>
-                                <IconButton style={myStyle.iconButton} onClick={this.handleClose}>
-                                    <CloseIcon />
-                                </IconButton>
+                                    <SmallIconButton key="text-page" onClick={this.handleClose} icon={<CloseIcon />} />
                                 </div>
                                 <div style={{textAlign:"center"}}>
                                     <h5>
@@ -201,16 +266,19 @@ class Home extends Component {
                                         onChange={(e) => {this.setState({currentText:e.target.value})}}
                                     />
                                     
-                                    {this.state.currentGif == ""?null:
+                                    {this.state.currentGif === ""?null:
                                     <Row>
                                         <Col s={12} style={{textAlign:"center", padding:"0"}}>
                                         <div style={{display: "inline-block",position: "relative", width:"100%"}}>
                                             <div style={{position:"absolute",right:0, top:0}}>
-                                                <IconButton style={{backgroundColor:"#D8DADF", margin:"8px", padding:"8px"}} onClick={() => this.setState({currentGif:""})}>
+                                                <IconButton 
+                                                    style={myStyle.removeGifButton} 
+                                                    onClick={() => this.setState({currentGif:""})}
+                                                >
                                                     <CloseIcon />
                                                 </IconButton>
                                             </div>
-                                            <img style={{borderRadius:"8px"}} width="100%" src={this.state.currentGif}></img>
+                                            <img style={{borderRadius:"8px"}} width="100%" src={this.state.currentGif} alt={"Gif"}></img>
                                         </div>
                                         </Col>
                                     </Row>
@@ -220,12 +288,12 @@ class Home extends Component {
                                         icon={<GifIcon />}
                                         label={"GIF"}
                                         clickable
-                                        style={this.state.currentGif == ""?myStyle.normalChip:myStyle.selectChip}
+                                        style={this.state.currentGif === ""?myStyle.normalChip:myStyle.selectChip}
                                         onClick={() => {this.setState({isGifPage:true})}}
                                     />
                                 </DialogContent>
                                 <DialogActions style={{padding:"8px 24px 24px 24px"}}>
-                                    <Button disabled={((this.state.currentText == "") && (this.state.currentGif == "") )?true:false} style={myStyle.submitButton} onClick={this.addPost}>
+                                    <Button disabled={((this.state.currentText === "") && (this.state.currentGif === "") )?true:false} style={myStyle.submitButton} onClick={this.addPost}>
                                         Post
                                     </Button>
                                 </DialogActions>
@@ -237,16 +305,16 @@ class Home extends Component {
                             return(
                                 <Card className="posts" key={index} style={myStyle.mainCard}>
                                     <Row style={myStyle.noMargin}>
-                                        {post.text == ""?null:
+                                        {post.text === ""?null:
                                             <Col s={12} style={myStyle.postText}>
                                                 {post.text}
                                             </Col>
                                         }
                                         
-                                        {post.gif == ""? null 
+                                        {post.gif === ""? null 
                                         :
                                             <Col s={12} style={myStyle.gifPost}>
-                                                <img style={{width:"100%"}} src={post.gif}></img>
+                                                <img style={{width:"100%"}} src={post.gif} alt={"Gif"}></img>
                                             </Col>
                                         }
                                         
